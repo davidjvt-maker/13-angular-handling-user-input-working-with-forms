e5196aa2f7c5a64f612cdbaec44cb94caedb1ae1
davidjvt-maker <davidjvieirat.ec@gmail.com>
2026-06-17 17:18:22 -0500

    new testssss

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,54 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, AbstractControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+  return { mustContainQuestionMark: true };
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
   form = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark]
+    }),
   })
+
+  get emailInvalid() {
+    return this.form.controls.email.invalid && this.form.controls.email.touched;
+  }
+
+  get passwordInvalid() {
+    return this.form.controls.password.invalid && this.form.controls.password.touched;
+  }
+
+  ngOnInit(): void {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+    if (savedForm) {
+      this.form.patchValue({ email: JSON.parse(savedForm).email });
+    }
+    const suscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (value) => {
+        window.localStorage.setItem('saved-login-form', JSON.stringify(value.email))
+      }
+    })
+    this.destroyRef.onDestroy(() => suscription.unsubscribe());
+
+
+  }
 
   onSubmit() {
     console.log('FORM SUBMITTED')
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,58 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormArray, AbstractControl } from '@angular/forms';
+
+function mustMatch(control: AbstractControl) {
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+  if (password !== confirmPassword) {
+    return { mustMatch: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+    passwords: new FormGroup({
+      password: new FormControl('', { validators: [Validators.required, Validators.minLength(6), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')] }),
+      confirmPassword: new FormControl('', { validators: [Validators.required, Validators.minLength(6), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')] }),
+    }, { validators: [mustMatch] }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
+    }),
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', { validators: [Validators.required] }),
+    source: new FormArray([
+      new FormControl<boolean>(false),
+      new FormControl<boolean>(false),
+      new FormControl<boolean>(false)
+    ]),
+    agree: new FormControl<boolean>(false, [Validators.requiredTrue])
+
+  })
+
+
+  onSubmit() {
+    if (this.form.invalid) {
+      console.log('Invalid form')
+      return;
+    }
+    console.log(this.form.value)
+
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+}
